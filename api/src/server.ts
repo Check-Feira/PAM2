1691d1af937284bdd2d3ae9f7f56d5fa35f40838
Andrew Costa <andrewcosta.contato@gmail.com>
2025-05-19 17:05:13 -0300

    feat: configura rotas

---
+++ api/src/server.ts
@@ -0,0 +1,19 @@
+require('dotenv').config();
+import { wishlistRoutes } from "./routes";
+
+const cors = require('cors');
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3333;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(wishlistRoutes);
+
+
+app.listen(PORT, () => {
+  console.log(`O servidor está rodando em http://localhost:${PORT}`);
+});
